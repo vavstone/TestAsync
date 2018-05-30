03b2e7da6beb6a6c9ae718e5fdc2d3ede1168639
Андрей Веселков <vavstone@list.ru>
2018-05-31 01:52:32 +0300

    Add task
    Add task

---
+++ TestAsyncConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestAsyncConsole
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Waiting for greeting...");
+            Console.WriteLine(GetHelloString());
+            Console.WriteLine("It's too late!");
+            Console.Read();
+        }
+
+        static string GetHelloString()
+        {
+            Thread.Sleep(1000);
+            return "Hello, App!";
+        }
+    }
+}
